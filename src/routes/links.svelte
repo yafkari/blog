b31b92bc7b4de8288af56e451fbba3f7253969b0
Younes Afkari <52196@etu.he2b.be>
2022-01-26 00:26:07 +0100

    move boxicon css file loading in links.svelte (where its used only)

--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -28,6 +28,7 @@
 
 <svelte:head>
   <title>Links - Younes Afkari</title>
+  <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
 </svelte:head>
 <div>
   <ul class="links">
